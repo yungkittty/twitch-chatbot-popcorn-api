e6ef89a7b1673cc2f7f8e38a18ac68d56262033b
Quentin Woivré <quentin.woivre@epitech.eu>
2020-01-08 15:27:41 +0100

    app-database, ...

--- src/configurations/app-database/app-database.js
+++ src/configurations/app-database/app-database.js
@@ -1,19 +1,12 @@
 const appDatabase = {
   users: {
-    "dffb7da0-5ef0-4edb-9a49-05268189281b": {
-      id: "dffb7da0-5ef0-4edb-9a49-05268189281b",
+    /* "": {
+      id: "",
       credentials: {
-        username: "yungkittty",
-        password: "oauth:kzoe5lem2qsgv0s9i41ulsdwqrzkv6"
+        username: "",
+        password: ""
       }
-    },
-    "70d256fc-8def-41cf-bb80-abbe879900f9": {
-      id: "70d256fc-8def-41cf-bb80-abbe879900f9",
-      credentials: {
-        username: "domingo",
-        password: "oauth:w26vy04907nysrpabwtnvvlzrg7ezc"
-      }
-    }
+    } */
   },
   watchers: {}
 };
